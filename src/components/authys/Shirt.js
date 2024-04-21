cdcac27ef3342e551cc312f910ae4037cdf804f6
Dhanasriganesh <dhanasriganesh9@gmail.com>
2024-04-21 08:43:58 +0530

    Inn-commit

--- src/components/authys/Shirt.js
+++ src/components/authys/Shirt.js
@@ -20,22 +20,26 @@ const Shirt = () => {
                 <FaArrowRight />
             </div>
             <div className='text-img'>
+                
+                
+                <div className='text-container'>
+                <h1>هابنيس إسطنبول قميص أبيض</h1>
+                    <div className='bottom-text'>
+                        <h2>trendyol من </h2>
+                        <p>المقاسات المناسبة لك</p>
+                        <p>الألوان المناسبة لك</p>
+                        <p> </p>
+                        <p>تفاصيل المنتج</p>
+                        <p>النمط: بيسك</p>
+                        <p>الطول: عادي</p>
+                        <p>طول الكم: أكمام طويلة</p>
+                        <p>القالب: مقاس عادي</p>
+                        <p>نوع القماش: بوليستر %100</p>
+                    </div>
+                </div>
                 <div className='shop'>
                     <img className="shirt"
                         src={shirt} alt="shirt" />
-                </div>
-                <h1>هابنيس إسطنبول قميص أبيض</h1>
-                <div className='text-container'>
-                    <h2>trendyol من </h2>
-                    <p>المقاسات المناسبة لك</p>
-                    <p>الألوان المناسبة لك</p>
-                    <p> </p>
-                    <p>تفاصيل المنتج</p>
-                    <p>النمط: بيسك</p>
-                    <p>الطول: عادي</p>
-                    <p>طول الكم: أكمام طويلة</p>
-                    <p>القالب: مقاس عادي</p>
-                    <p>نوع القماش: بوليستر %100</p>
                 </div>
             </div>
 
