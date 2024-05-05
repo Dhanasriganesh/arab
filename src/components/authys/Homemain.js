9054abfcc78f0b7e8e4e7dfafd6e931462f18e47
Dhanasriganesh <dhanasriganesh9@gmail.com>
2024-05-05 22:36:07 +0530

    resp

--- src/components/authys/Homemain.js
+++ src/components/authys/Homemain.js
@@ -15,7 +15,7 @@ import frock from "../../Assets/5.jpg"
 import shirt from "../../Assets/7.jpg"
 const Homemain = () => {
   return (
-    <div className='auth-box'>
+    
       <div className='homepage'>
         <div className='nav'>
           <div className='icons'>
@@ -116,7 +116,7 @@ const Homemain = () => {
         </div>
       
       </div>
-    </div>
+    
   )
 }
 
